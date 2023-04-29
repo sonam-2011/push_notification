adf2be7dda50031be62a608a32fa2b7c0721f64d
sonam-2011 <sonamrastogi.20nov@gmail.com>
2023-04-29 16:19:05 +0530

    firebase implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'homepage.dart';
+import 'notificationservice/local_notification_service.dart';
 
-void main() {
+
+
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  LocalNotificationService.initialize();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
 
   // This widget is the root of your application.
   @override
